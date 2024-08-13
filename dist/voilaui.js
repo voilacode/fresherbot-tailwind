34836e32538c54b5b15255044e2ad2423679fb29
shifamahveen <mahveenshifa1611@gmail.com>
2024-08-13 16:56:47 +0530

    rebase

--- dist/voilaui.js
+++ dist/voilaui.js
@@ -158,3 +158,99 @@ style.innerHTML = `
     }
 `;
 document.head.appendChild(style);
+
+
+/** 
+ * Accordion script to handle multiple accordions 
+ * that have class name accordion 
+*/
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+
+        // Close all other panels
+        let allPanels = document.getElementsByClassName("panel");
+        for (let j = 0; j < allPanels.length; j++) {
+            if (allPanels[j] !== panel) {
+                allPanels[j].style.display = "none";
+                // Remove "active" class from other buttons
+                acc[j].classList.remove("active");
+                // Reset the icon for other buttons
+                resetIcon(acc[j]);
+            }
+        }
+
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+            resetIcon(this);
+        } else {
+            panel.style.display = "block";
+            toggleIcon(this);
+        }
+
+    });
+}
+
+function resetIcon(element) {
+    let icon = element.querySelector('.icon');
+    if (icon) {
+        icon.innerHTML = '+';
+    }
+}
+
+function toggleIcon(element) {
+    let icon = element.querySelector('.icon');
+    if (icon) {
+        icon.innerHTML = '-';
+    }
+}
+
+// scroll to top
+window.addEventListener('scroll', function () {
+    var scrollToTopButton = document.getElementById('scrollToTop');
+    if (window.scrollY > 200) {
+        scrollToTopButton.classList.remove('opacity-0');
+    } else {
+        scrollToTopButton.classList.add('opacity-0');
+    }
+});
+
+document.getElementById('scrollToTop').addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+
+// load effect
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('section');
+
+  const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('opacity-100');
+              entry.target.classList.remove('opacity-0');
+          } else {
+              entry.target.classList.remove('opacity-100');
+              entry.target.classList.add('opacity-0');
+          }
+      });
+  }, observerOptions);
+
+  sections.forEach(section => {
+      observer.observe(section);
+  });
+});
+
