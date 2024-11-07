2da06a45f10439df52c04147c18d79b80155b4bb
shifamahveen <mahveenshifa1611@gmail.com>
2024-11-07 17:01:21 +0530

    index versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,18 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'purple-custom': "#1e1938",
+        'purple-lite': '#A64D79',
+        'purple-text': '#FFE3E3',
+      },
       fontFamily: {
         'open': ['Open Sans', 'sans-serif'],
         'noto': ['Noto Serif', 'serif'],
         'archivo': ["Archivo", 'sans-serif'],
         'julius': ["Julius Sans One", 'sans-serif'],
         'poppins': ["Poppins", 'sans-serif'],
+        'roboto': ["Roboto", 'sans-serif'],
     }
     },
   },
